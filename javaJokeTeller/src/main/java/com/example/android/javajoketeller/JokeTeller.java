620cc2d6e46d42816b470ccc3e5db6e9337465d1
eslam <eslamelmishtawy@gmail.com>
2018-10-07 15:25:26 +0200

    ....

--- javaJokeTeller/src/main/java/com/example/android/javajoketeller/JokeTeller.java
+++ javaJokeTeller/src/main/java/com/example/android/javajoketeller/JokeTeller.java
@@ -9,10 +9,12 @@ public class JokeTeller {
 
     public JokeTeller() {
         jokes = new String[3];
-        jokes[0] = "Q: Why do programmers always mix up Halloween and Christmas?\n" +
+        jokes[0] = "Q: What's programmers favourit place?\n" +
                 "\n" +
-                "A: Because Oct 31 == Dec 25!";
-        jokes[1] = "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"";
+                "A: Foo Bar";
+        jokes[1] = "Q: \"What do you call a programmer from Filland\"\n" +
+                "\n" +
+                "A: Nerdic";;
         jokes[2] = "Q: \"Whats the object-oriented way to become wealthy?\"\n" +
                 "\n" +
                 "A: Inheritance";
